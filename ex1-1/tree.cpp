40199f22e7736b3ab7de9610c4a4d8165c458d17
Shuntatsu <148511409+shuntatsu@users.noreply.github.com>
2024-06-10 17:37:52 +0900

    Add files via upload

--- ex1-1/tree.cpp
+++ ex1-1/tree.cpp
@@ -14,6 +14,7 @@ class Variable
 public:
     float a = 0;
     Function *creator = nullptr;
+    string name;
 
     // コンストラクタ 
     Variable(float a)
@@ -59,7 +60,7 @@ public:
 
 void traverse(PVariable v)
 {
-    cout << v->a << endl;
+    cout << "Variable " << v->name << " with value: " << v->a << " (use_count: " << v.use_count() << ")" << endl;
 
     Function *f = v->creator;
     if(f == NULL)
@@ -70,23 +71,38 @@ void traverse(PVariable v)
     for(int i=0; i < f->v.size(); i++)
     {
         traverse(f->v[i]);
-        cout << "size:" << f->v.size() << endl;
+        cout << "Variable :" << v->name << " ,size:" << f->v.size() << endl;
     }
 }
 
 int main(void)
 {
     PVariable v1 = PVariable(new Variable(1));
+    v1->name = "v1";
     PVariable v2 = PVariable(new Variable(2));
+    v2->name = "v2";
+    //v1とv2の参照カウントはそれぞれ1です。
+    cout << "v1 use_count: " << v1.use_count() << endl;
     Function *f1 = new Function();
     Function *f2 = new Function();
     Function *f3 = new Function();
 
     PVariable r1 = f1->forward(v1, v2);
+    r1->name = "r1";
     PVariable r2 = f2->forward(r1);
+    r2->name = "r2";
     PVariable r3 = f3->forward(r2);
+    r3->name = "r3";
 
     traverse(r3);
+
+    // 参照カウントとサイズの確認
+    cout << "After traverse:" << endl;
+    cout << "v1 use_count: " << v1.use_count() << endl;
+    cout << "v2 use_count: " << v2.use_count() << endl;
+    cout << "r1 use_count: " << r1.use_count() << endl;
+    cout << "r2 use_count: " << r2.use_count() << endl;
+    cout << "r3 use_count: " << r3.use_count() << endl;
     return 0;
 }
 
@@ -112,4 +128,36 @@ r3 (3)
 size: 1
 
 v1, v2はFunctionでないため、繰り返さない。
+
+f->v.size()
+f->v.size()は、Functionクラスのメンバ変数vのサイズを返します。
+vはvector<PVariable>型で、Functionオブジェクトが保持しているPVariableの数を示します。
+
+実行結果
+Variable r3 with value: 3 (use_count: 2)
+Variable r2 with value: 3 (use_count: 3)
+Variable r1 with value: 3 (use_count: 3)
+Variable v1 with value: 1 (use_count: 3)
+Variable :r1 ,size:2
+Variable v2 with value: 2 (use_count: 3)
+Variable :r1 ,size:2
+Variable :r2 ,size:1
+Variable :r3 ,size:1
+After traverse:
+v1 use_count: 2
+v2 use_count: 2
+r1 use_count: 2
+r2 use_count: 2
+r3 use_count: 1
+f1->v.size(): 2
+f2->v.size(): 1
+f3->v.size(): 1
+
 */
+
+
+/*
+循環参照の問題
+このコードでは、FunctionオブジェクトがVariableオブジェクトをshared_ptrで保持し、VariableオブジェクトがFunctionオブジェクトを指すポインタを持っています。
+このように、オブジェクトが互いに参照し合うと、参照カウントが0にならず、メモリが解放されません。
+ */
